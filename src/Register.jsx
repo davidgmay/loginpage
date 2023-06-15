e9bf8f4f13162678e17cc347e279b27a44b142d5
davidgmay <111102775+davidgmay@users.noreply.github.com>
2023-06-15 15:51:07 -0500

    register button updated

--- src/Register.jsx
+++ src/Register.jsx
@@ -20,7 +20,7 @@ export const Register = (props) => {
                 <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
                 <label htmlFor="password">Password</label>
                 <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-                <button type="submit">Log In</button>
+                <button type="submit">Register</button>
             </form>
             <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
         </div>
